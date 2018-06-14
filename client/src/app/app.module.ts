83f1e7ec9e671aaca256f2561aa2bc3377b564c2
saibot777 <stefantrajkovic777@gmail.com>
2018-06-14 12:44:47 +0200

    architecture update

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "./material.module";
+import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
